8d18502bb818bb5868fdff0c313332657a877ab9
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-05-19 19:41:37 +0900

    feat: 3-3-todo-apiを追加

---
+++ 3-sqlite-server/3-3-todo-api/queries.js
@@ -0,0 +1,15 @@
+export const Tasks = {
+  createTable: `
+    CREATE TABLE IF NOT EXISTS tasks (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      completed BOOLEAN NOT NULL
+    );
+  `,
+  create: `INSERT INTO tasks (title, completed) VALUES (?, ?);`,
+  readAll: `SELECT * FROM tasks;`,
+  readById: `SELECT * FROM tasks WHERE id = ?;`,
+  setCompleteStateById: `UPDATE tasks SET completed = ? WHERE id = ?;`,
+  updateTitleById: `UPDATE tasks SET title = ? WHERE id = ?;`,
+  deleteById: `DELETE FROM tasks WHERE id = ?;`,
+};
---
+++ 3-sqlite-server/3-3-todo-api/server.js
@@ -0,0 +1,124 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import sqlite3 from "sqlite3";
+import * as queries from "./queries.js";
+
+const todo = new Hono();
+
+todo.use(
+  "*",
+  cors({
+    origin: "null",
+  })
+);
+
+const db = new sqlite3.Database("test.db");
+
+const migrate = () => {
+  db.serialize(() => {
+    db.run(queries.Tasks.createTable);
+  });
+}
+
+todo.get("/", async (c) => {
+  const tasks = await new Promise((resolve) => {
+    db.all(queries.Tasks.readAll, (err, rows) => {
+      resolve(rows);
+    });
+  });
+
+  return new Response(JSON.stringify(tasks), {
+    headers: { "Content-Type": "application/json" },
+  });
+});
+
+todo.post("/", async (c) => {
+  const param = await c.req.json();
+
+  await new Promise((resolve) => {
+    db.run(queries.Tasks.create, param.title, param.completed || false, (err) => {
+      if (err) {
+      throw err;
+      }
+      resolve();
+    });
+  }).catch((err) => {
+    return new Response(JSON.stringify({ message: err.message }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  });
+
+  return new Response(null, { status: 201 });
+});
+
+todo.put("/:id", async (c) => {
+  const param = await c.req.json();
+  const id = c.req.param("id");
+
+  if (param.title !== undefined) {
+    await new Promise((resolve) => {
+      db.run(queries.Tasks.updateTitleById, param.title, id, (err) => {
+        if (err) {
+          throw err;
+        }
+        resolve();
+      });
+    }).catch((err) => {
+      return new Response(JSON.stringify({ message: err.message }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    });
+  }
+
+  if (param.completed !== undefined) {
+    await new Promise((resolve) => {
+      db.run(queries.Tasks.setCompleteStateById, param.completed ? "1" : "0", id, (err) => {
+        if (err) {
+          throw err;
+        }
+        resolve();
+      });
+    }).catch((err) => {
+      return new Response(JSON.stringify({ message: err.message }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    });
+  }
+
+  return new Response(null, { status: 204 });
+});
+
+todo.delete("/:id", async (c) => {
+  const id = c.req.param("id");
+
+  await new Promise((resolve) => {
+    db.run(queries.Tasks.deleteById, id, (err) => {
+      if (err) {
+        throw err;
+      }
+      resolve();
+    });
+  }).catch((err) => {
+    return new Response(JSON.stringify({ message: err.message }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  });
+
+  return new Response(null, { status: 204 });
+});
+
+const app = new Hono();
+
+app.route("/api/todo", todo);
+
+migrate();
+
+serve({
+  fetch: app.fetch,
+  port: 8000,
+});
