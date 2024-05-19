ee861a0f332d86abc7a0e804e9c85064697772cb
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-05-19 23:37:59 +0900

    refactor: クエリを全てserver.jsに移動

--- 3-sqlite-server/3-1-simple-sqlite-app/server.js
+++ 3-sqlite-server/3-1-simple-sqlite-app/server.js
@@ -1,17 +1,29 @@
 import sqlite3 from "sqlite3";
-import * as queries from "./queries.js";
 
 const db = new sqlite3.Database("test.db");
 
+// テーブル作成するSQL
+const createTable = `
+CREATE TABLE IF NOT EXISTS tasks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  completed BOOLEAN NOT NULL
+);
+`;
+// タスクを追加するSQL
+const createTask = `INSERT INTO tasks (title, completed) VALUES (?, ?);`;
+// タスクを全て取得するSQL
+const readAllTasks = `SELECT * FROM tasks;`;
+
 db.serialize(() => {
   // tasksテーブルが存在しない場合は作成
-  db.run(queries.Tasks.createTable);
+  db.run(createTable);
 
   // タスクを追加
-  db.run(queries.Tasks.create, "SQLiteの操作に慣れる", false);
+  db.run(createTask, "SQLiteの操作に慣れる", false);
 
   // タスクを全て取得
-  db.each(queries.Tasks.readAll, (err, row) => {
+  db.each(readAllTasks, (err, row) => {
     if (err) {
       throw err;
     }
