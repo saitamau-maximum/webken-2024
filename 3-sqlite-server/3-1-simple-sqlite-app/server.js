1b53f473b14ea2fad2179ef02792508ea8ba511e
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-05-19 23:48:22 +0900

    chore: コメントを変更

--- 3-sqlite-server/3-1-simple-sqlite-app/server.js
+++ 3-sqlite-server/3-1-simple-sqlite-app/server.js
@@ -2,7 +2,7 @@ import Database from "better-sqlite3";
 
 const db = new Database("test.db");
 
-// テーブル作成するSQL
+// tasksテーブルが存在しない場合は作成するSQL
 const createTable = `
 CREATE TABLE IF NOT EXISTS tasks (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,20 +10,14 @@ CREATE TABLE IF NOT EXISTS tasks (
   completed BOOLEAN NOT NULL
 );
 `;
-
 // タスクを追加するSQL
 const createTask = `INSERT INTO tasks (title, completed) VALUES (?, ?);`;
-
 // タスクを全て取得するSQL
 const readAllTasks = `SELECT * FROM tasks;`;
 
-// tasksテーブルが存在しない場合は作成
+// クエリを実行
 db.prepare(createTable).run();
-
-// タスクを追加
 db.prepare(createTask).run("SQLiteの操作に慣れる", "0");
-
-// タスクを全て取得
 const tasks = db.prepare(readAllTasks).all();
 
 console.log(tasks);
