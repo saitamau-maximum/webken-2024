5e58b4fa96da67835c59040d658b18d8cb580fd7
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-05-20 19:10:39 +0900

    feat: transactionを使わないように変更

--- 3-sqlite-server/3-2-alter-tasks/server.js
+++ 3-sqlite-server/3-2-alter-tasks/server.js
@@ -12,31 +12,27 @@ const displayAllTasks = (title) => {
   console.log();
 };
 
-db.transaction(() => {
-  // tasksテーブルが存在しない場合は作成
-  db.exec(Tasks.createTable);
+// tasksテーブルが存在しない場合は作成
+db.exec(Tasks.createTable);
 
-  // タスクを追加
-  db.prepare(Tasks.create).run("SQLiteの操作に慣れる", 0);
-  db.prepare(Tasks.create).run("TODOアプリを完成させる", 0);
-  db.prepare(Tasks.create).run("ICPCに参加する", 0);
-})();
+// タスクを追加
+db.prepare(Tasks.create).run("SQLiteの操作に慣れる", 0);
+db.prepare(Tasks.create).run("TODOアプリを完成させる", 0);
+db.prepare(Tasks.create).run("ICPCに参加する", 0);
 
 // タスクを全て取得
 displayAllTasks("初期状態");
 
-db.transaction(() => {
-  // タスクを完了にする
-  db.prepare(Tasks.setCompleteStateById).run(1, 1);
-  displayAllTasks("タスク1を完了にした状態");
+// タスクを完了にする
+db.prepare(Tasks.setCompleteStateById).run(1, 1);
+displayAllTasks("タスク1を完了にした状態");
 
-  // タスクのタイトルを更新
-  db.prepare(Tasks.updateTitleById).run("Node.jsの操作に慣れること", 1);
-  displayAllTasks("タスク1のタイトルを更新した状態");
+// タスクのタイトルを更新
+db.prepare(Tasks.updateTitleById).run("Node.jsの操作に慣れること", 1);
+displayAllTasks("タスク1のタイトルを更新した状態");
 
-  // タスクを削除
-  db.prepare(Tasks.deleteById).run(2);
-  displayAllTasks("タスク2を削除した状態");
-})();
+// タスクを削除
+db.prepare(Tasks.deleteById).run(2);
+displayAllTasks("タスク2を削除した状態");
 
 db.close();
