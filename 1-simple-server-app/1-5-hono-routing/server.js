7e9474d969137d6afafcf3f8fdde4e6a718699d5
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2024-05-21 16:39:55 +0900

    fix: 正規表現を用いたroutingを削除

--- 1-simple-server-app/1-5-hono-routing/server.js
+++ 1-simple-server-app/1-5-hono-routing/server.js
@@ -16,13 +16,6 @@ app.get("/todo/", (c) => c.json(todoList, 200));
 // GET /:id
 app.get("/todo/:id", (c) => c.json("入力されたID: " + c.req.param("id"), 200));
 
-// パス名には正規表現を使うことができる
-// https://hono.dev/api/routing
-app.get("/todo/:date{[0-9]+}/:title{[a-z]+}", (c) => {
-  const { date, title } = c.req.param();
-  return c.json({ date, title }, 200);
-});
-
 serve({
   fetch: app.fetch,
   port: 8000,
